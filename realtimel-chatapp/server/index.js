680d98f5b65cefe36e35d2964aa4860ff9760b94
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-08-06 10:13:13 +0900

    commit

---
+++ realtimel-chatapp/server/index.js
@@ -0,0 +1,30 @@
+const { Socket } = require("dgram");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3001"],
+  },
+});
+
+const PORT = 5001;
+
+io.on("connection", (socket) => {
+  console.log("クライアントと接続しました");
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+
+    io.emit("res_message" ,data)
+  });
+
+  socket.on("disconnect", () => {
+    console.log("クライアントと接続が切れました");
+  });
+});
+
+server.listen(PORT, () => console.log(`server is running on${PORT}`));
